a8e59bc38683ac07f1be809c5dbe1efa465fe269
m-yazdanpanaah <78688077+m-yazdanpanaah@users.noreply.github.com>
2021-06-07 21:11:31 +0430

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,20 +5,14 @@ import Link from 'next/link';
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = ({}) => {
-  return (
-    <nav className="pt-8 flex md:justify-between text-ocean h-16 relative z-10">
-      <p className="text-xl font-bold text-hearth"/>
-      <ul className="gap-8 mr-4 hidden md:flex">
-          <SmoothScroll text="Portfolio" to="portfolio" />
-          <SmoothScroll text="About" to="about" />
-          <SmoothScroll text="Contact me" to="contact" />
-
-
-
-
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="pt-8 flex md:justify-between text-ocean h-16 relative z-10">
+            <p className="text-xl font-bold text-hearth">
+            Mojtaba</p>
+            <ul className="gap-8 mr-4 hidden md:flex">
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
